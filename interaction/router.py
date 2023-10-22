dc97a18707b19a07eedf9366e81a7a0da6dae0de
Casey <caseyjohnsonwv@gmail.com>
2023-10-21 21:58:52 -0400

    add runtime file to tell heroku python version

--- interaction/router.py
+++ interaction/router.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 import uuid
 from fastapi import APIRouter, Request, Response, HTTPException, status as HTTPStatusCode
@@ -143,10 +144,7 @@ async def interaction(req:Request):
         styles_list = Command.styles()
         styles_fmt = [f"{i+1}. {s}" for i,s in enumerate(styles_list)]
         # split into columns for condensed tabular display
-        cols = [None]*3
-        col_len = sum(divmod(len(styles_fmt), len(cols)))
-        for i in range(0, len(styles_fmt), col_len):
-            col = styles_fmt[i*col_len : min(i+col_len, len(styles_fmt))]
+        for col in itertools.batched(styles_fmt, 3):
             fields.append({'name':'', 'value': '\n'.join(col), 'inline':True})
 
     elif command == Command.UNSUBSCRIBE:
