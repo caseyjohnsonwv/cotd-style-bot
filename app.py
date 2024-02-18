0f2b0e2e4e7aa2954e0d1b2f2797d48b15748057
Casey Johnson <caseyjohnsonwv@gmail.com>
2024-02-18 16:17:12 -0500

    change datatype for big integers

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ LAST_RESTART_TIME = datetime.utcnow()
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup - create db tables if not exist
+    db.drop_all()
     db.create_all()
-    print(db.populate_style_table())
+    db.populate_style_table()
     # yield to let application run
     yield
 
