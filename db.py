a9b8de47615dce3f355c0f459b453c457079ca12
Casey Johnson <caseyjohnsonwv@gmail.com>
2024-02-18 16:22:52 -0500

    trying to fix bigint still

--- db.py
+++ db.py
@@ -48,9 +48,9 @@ def populate_style_table() -> int:
 class Subscription(Base):
     __tablename__ = 'subscription'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    guild_id: Mapped[BigInteger] = mapped_column(nullable=False)
-    channel_id: Mapped[BigInteger] = mapped_column(nullable=False)
-    role_id: Mapped[BigInteger] = mapped_column(nullable=False)
+    guild_id: Mapped[int] = mapped_column(type=BigInteger, nullable=False)
+    channel_id: Mapped[int] = mapped_column(type=BigInteger, nullable=False)
+    role_id: Mapped[int] = mapped_column(type=BigInteger, nullable=False)
     style_id: Mapped[int] = mapped_column(ForeignKey('style.id'))
 
 def create_subscription(guild_id:int, channel_id:int, role_id:int, style_name:str) -> int:
